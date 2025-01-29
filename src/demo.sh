2f28e0e1a89bc2c73424174d3af0efdf61aae873
Jagrati Talreja <86330025+JagratiTalreja01@users.noreply.github.com>
2025-01-29 15:53:47 -0500

    Add files via upload

---
+++ src/demo.sh
@@ -0,0 +1,16 @@
+
+python src/main.py --dir_data ../Dataset/ --model HNCT --data_test Set5+Set14+B100+Urban100+Manga109  --data_range 801-900 --scale 4 --save HNCT_x4 --pre_train /home/vtrg/Desktop/Jagrati2024/HNCT/experiment/test/x4/x4.pt --rgb_range 1 --test_only --save_results
+
+python src/main.py --dir_data ../Dataset/ --model HNCT --data_test Set5+Set14+B100+Urban100+Manga109  --data_range 801-900 --scale 3 --save HNCT_x3 --pre_train /home/vtrg/Desktop/Jagrati2024/HNCT/experiment/test/x3/x3.pt --rgb_range 1 --test_only --save_results
+
+python src/main.py --dir_data ../Dataset/ --model HNCT --data_test Set5+Set14+B100+Urban100+Manga109  --data_range 801-900 --scale 2 --save HNCT_x2 --pre_train /home/vtrg/Desktop/Jagrati2024/HNCT/experiment/test/x2/x2.pt --rgb_range 1 --test_only --save_results
+
+/home/vtrg/Desktop/Jagrati2024/experiment/test/model
+
+python src/main.py --dir_data ../Dataset/ --model dhtcun --data_test Set5+Set14+B100+Urban100+Manga109  --data_range 801-900 --scale 2 --save HNCT_x2 --pre_train /home/vtrg/Desktop/Jagrati2024/experiment/test/model/model_best.pt --rgb_range 1 --test_only --save_results
+
+python src/main.py --dir_data ../Dataset/ --model dhtcun --data_test Set5+Set14+B100+Urban100+Manga109  --data_range 801-900 --scale 4 --save HNCT_x4 --pre_train /home/vtrg/Desktop/Jagrati2024/experiment/test/model/model_best.pt --rgb_range 1 --test_only --save_results
+
+python src/main.py --dir_data ../Dataset/ --model dhtcun --data_test Set5+Set14+B100+Urban100+Manga109  --data_range 801-900 --scale 3 --save HNCT_x3 --pre_train /home/vtrg/Desktop/Jagrati2024/experiment/test/model/model_best.pt --rgb_range 1 --test_only --save_results
+
+python src/main.py --dir_data ../Dataset/ --model HNCT --data_test Set5+Set14+B100+Urban100+Manga109  --data_range 801-900 --scale 4 --save HNCT_x4 --pre_train /home/vtrg/Desktop/Jagrati2024/HNCT/experiment/test/x4/x4.pt --rgb_range 1 --test_only --save_results
